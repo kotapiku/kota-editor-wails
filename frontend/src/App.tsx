453bc8f16f2935eb56440bc4170dedd3712a6d18
kotapiku <ice08235523@gmail.com>
2023-07-11 22:15:42 +0900

    feat: change layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,77 @@
 import React, { useEffect, useState } from "react";
 import { Editor } from "./components/Editor";
-import { OpenDirectory, SelectFile } from "../wailsjs/go/main/App";
+import { OpenDirectory } from "../wailsjs/go/main/App";
 import { main } from "../wailsjs/go/models";
-import { Tree, Breadcrumb, Button, Layout, Menu } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
-import type { DataNode, DirectoryTreeProps } from "antd/es/tree";
+import { Row, Divider, Typography, Button, Layout, Menu } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { MenuInfo } from "rc-menu/lib/interface";
+import { FolderOutlined, FileOutlined } from "@ant-design/icons";
 import "./App.css";
 
 const { Header, Content, Sider } = Layout;
-const { DirectoryTree } = Tree;
+type MenuItem = Required<MenuProps>["items"][number];
+const { Text } = Typography;
 
-function fromFileNode(node: main.FileNode): DataNode {
+// if not md then disabled: true
+function fromFileToMenuItem(node: main.FileNode): MenuItem {
   return {
-    title: node.current_dir.basename,
     key: node.current_dir.absolute_path,
-    selectable: !node.is_dir,
-    isLeaf: !node.is_dir,
-    children: node.children.map((child) => fromFileNode(child)),
+    icon: node.is_dir ? <FolderOutlined /> : <FileOutlined />,
+    label: node.current_dir.basename,
+    children: node.is_dir
+      ? node.children.map((child) => fromFileToMenuItem(child))
+      : undefined,
   };
 }
 
 function App() {
   const [filePath, setFilePath] = useState<string>();
-  const [treeData, setTreeData] = useState<DataNode[]>([]);
-
-  const onClick = async () => {
-    let file = await SelectFile();
-    console.log("selected file: ", file.absolute_path);
-    setFilePath(file.absolute_path);
-  };
+  const [items, setItems] = useState<MenuItem[]>([]);
 
   const onClickDir = async () => {
     let dir = await OpenDirectory();
     console.log("selected file: ", dir.current_dir);
-    setTreeData([...treeData, fromFileNode(dir)]);
+    setItems([...items, fromFileToMenuItem(dir)]);
   };
 
-  const onSelect: DirectoryTreeProps["onSelect"] = (keys, info) => {
-    console.log("Trigger Select", keys, info);
-    setFilePath(info.selectedNodes[0].key as string);
-  };
-
-  const onExpand: DirectoryTreeProps["onExpand"] = (keys, info) => {
-    console.log("Trigger Expand", keys, info);
+  const onClickFile: MenuProps["onClick"] = (e: MenuInfo) => {
+    console.log("selected file: ", e.key);
+    setFilePath(e.key);
   };
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider>
-        <Button icon={<UploadOutlined />} onClick={onClickDir}>
-          Open directory
-        </Button>
-        <DirectoryTree
-          defaultExpandAll
-          onSelect={onSelect}
-          onExpand={onExpand}
-          treeData={treeData}
-        />
+      <Sider theme="light">
+        <div className="sidebar">
+          <div className="projects">
+            <Row
+              style={{
+                "justify-content": "space-between",
+                "align-items": "center",
+              }}
+            >
+              <Text strong type="secondary">
+                Projects
+              </Text>
+              <Button
+                icon={<PlusOutlined />}
+                type="text"
+                onClick={onClickDir}
+              />
+            </Row>
+          </div>
+          <Divider style={{ "margin-bottom": "0px", "margin-top": "0px" }} />
+          <Menu
+            onClick={onClickFile}
+            theme="light"
+            mode="inline"
+            items={items}
+          />
+        </div>
       </Sider>
       <Layout>
-        <Content style={{ margin: "0 16px" }}>
+        <Content>
           <Editor filepath={filePath} />
         </Content>
         {/* <Footer style={{ textAlign: "center" }}>
